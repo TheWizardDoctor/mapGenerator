4394791f007d8ec518ce89820326d805f7ea32b4
TylerHoerschgen <77707612+TylerHoerschgen@users.noreply.github.com>
2022-02-17 13:11:51 -0600

    modified camera and city generation
    camera should now scale with size of map and city generation has been improved to seem more realistic

--- MapGenerator/Assets/Scripts/CreateCities.cs
+++ MapGenerator/Assets/Scripts/CreateCities.cs
@@ -8,7 +8,7 @@ public class CreateCities : MonoBehaviour
     const int scanSize = 1;
     const int oceanVal = 30;
 
-
+    //create num number of cities
     public static void generateCities(Tile[,] t, int num)
     {
         while(num>0)
@@ -18,6 +18,7 @@ public class CreateCities : MonoBehaviour
         }
     }
 
+    //create a single city
     public static void generateCity(Tile[,] tiles)
     {
         int bestVal = int.MinValue;
@@ -45,6 +46,7 @@ public class CreateCities : MonoBehaviour
         city.transform.position = new Vector3(bestTile.X, ((bestTile.Elevation+10)/2)+1, bestTile.Y);
     }
 
+    //get value of current tile based on neighboring tiles
     public static int calculateValue(Tile[,] tiles, Tile tile)
     {
         int scan = scanNearby(tiles, tile);
@@ -56,19 +58,43 @@ public class CreateCities : MonoBehaviour
     {
         int value = 0;
 
+        //left
         Tile scan = tile.left;
         value += biomeValue(scan);
         value += hasCity(scan);
 
+        //left up diag
+        scan = scan.up;
+        value += biomeValue(scan);
+        value += hasCity(scan);
+
+        //up
         scan = tile.up;
         value += biomeValue(scan);
         value += hasCity(scan);
 
+        //right diag up
+        scan = scan.right;
+        value += biomeValue(scan);
+        value += hasCity(scan);
+
+        //right
         scan = tile.right;
         value += biomeValue(scan);
         value += hasCity(scan);
 
+        //right diag down
+        scan = scan.down;
+        value += biomeValue(scan);
+        value += hasCity(scan);
+
+        //down
         scan = tile.down;
+        value += biomeValue(scan);
+        value += hasCity(scan);
+
+        //left diag down
+        scan = scan.left;
         value += biomeValue(scan);
         value += hasCity(scan);
 
@@ -79,16 +105,26 @@ public class CreateCities : MonoBehaviour
     {
         switch (tile.Biome)
         {
-            case Biome.TemperateForest:
-                return 100;
-            case Biome.Ocean:
-                return 30;
             case Biome.BorealForest:
                 return 20;
             case Biome.Desert:
                 return -20;
             case Biome.Mountain:
                 return -10;
+            case Biome.Ocean:
+                return 30;
+            case Biome.Prairie:
+                return 20;
+            case Biome.Rainforest:
+                return 25;
+            case Biome.Savannah:
+                return 20;
+            case Biome.Shrubland:
+                return 10;
+            case Biome.TemperateForest:
+                return 25;
+            case Biome.Tundra:
+                return 5;
             default:
                 return 0;
         }
--- MapGenerator/Assets/Scripts/Test.cs
+++ MapGenerator/Assets/Scripts/Test.cs
@@ -7,12 +7,15 @@ using UnityEngine;
 public class Test : MonoBehaviour
 {
     public GameObject tilePrefab;
+	public Camera cam;
     int width = 100;
     int height = 100;
 
     // Start is called before the first frame update
     void Start()
     {
+		cam.transform.position = new Vector3(height/2, height, width/2);
+
 		GameObject tileSet = new GameObject("Tiles");
 		
         Tile[,] t = new Tile[height, width];
@@ -96,6 +99,8 @@ public class Test : MonoBehaviour
 			}
 		}
 
+		//very simplistic city creation
+		//(currently only checks 8 nearby tiles to get tile's creation value)
         CreateCities.generateCities(t, 10);
         
     }
